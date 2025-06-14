ffd35ac55e594419981407f0a844f316bbe65ea6
Sarthak Singh <sarthaksingh1836@gmail.com>
2025-06-14 21:49:03 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,19 @@ function App() {
   
   const getBotResponse = (userInput)=>{
     const input = userInput.toLowerCase();
-
-    if(input.includes('hi') || input.includes('hello') || input.includes("hey there")) return "hello there! 😊";
+    if(input.includes("which date were you created") || input.includes("when were you created")) return "I don't know specifically about my creation date";
+    if(input.includes(' hi ') || input.includes('hello') || input.includes("hey there")) return "hello there! 😊";
    if(input.includes('how are you') ||  input.includes("whats up") ) return "I am good bro 😎. ";
-    if(input.includes('what is your name')) return "I am a chat bot ";
+    if(input.includes('what is your name') || input.includes("are you real")) return "I am a chat bot ";
     if(input.includes("who made you") || input.includes("who created you")) return "My creator is Sarthak";
     if(input.includes("tell me a joke")) return "Why are mountains so funny? They’re hill areas😂";
     if(input.includes("you have emotions")) return "No, i am Automated robot.";
     if(input.includes('bye') || input.includes("talk to you later") || input.includes("goodbye") || input.includes("byy")) return "see you! Bye";
     if(input.includes("where are you from")) return "The internet";
+    if(input.includes("is this conversation recorded")) return "No , it isn't.";
+    if(input.includes("use this chatbot on phone")) return "Yes, i am fully compatible with mobile devices.";
+    if(input.includes("what can you do")) return "I can answer basic questions as i am just a newbie🤓";
+    if(input.includes("what languages do you")) return "Right now, I can speak English only."
 
     return "sorry, i am unable to understand your message";
   }
